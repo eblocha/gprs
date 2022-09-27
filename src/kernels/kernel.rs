53c8882959c58fbd1489d7f9eaa921cd59c2a787
eblocha <eblocha@protonmail.com>
2022-09-26 20:10:23 -0400

    add docs to all kernel trait methods

--- src/kernels/kernel.rs
+++ src/kernels/kernel.rs
@@ -6,7 +6,10 @@ pub type CovarianceResult<T> = Result<T, CovarianceParamLengthError>;
 pub trait Kernel<P> {
     /// Compute the covariance
     fn call<'x, 'y>(&self, x: &'x Vec<f64>, y: &'y Vec<f64>) -> CovarianceResult<f64>;
+    /// Get the kernel parameters
     fn get_params(&self) -> P;
+    /// Set the kernel parameters
     fn set_params(&mut self, params: P);
+    /// Create a new kernel from parameters
     fn from_params(params: P) -> Self;
 }
