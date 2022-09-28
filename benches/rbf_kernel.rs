96de8bce2c0a9750a11cc47b2aa728224f3fb26d
eblocha <eblocha@protonmail.com>
2022-09-28 09:08:25 -0400

    increase benchmark matrix size to 2000x2000

--- benches/rbf_kernel.rs
+++ benches/rbf_kernel.rs
@@ -5,23 +5,28 @@ use nalgebra::{DMatrix, DVector};
 fn create_random(shape: (usize, usize)) -> DMatrix<f64> {
     DMatrix::<f64>::new_random(shape.0, shape.1)
 }
+const SZ: usize = 2000;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let kern = RBF::new(DVector::from_vec(vec![1.0, 2.0]));
-    let x = black_box(create_random((1000, 2)));
-    let mut raw = DMatrix::zeros(1000, 1000);
+
+    let x = create_random((SZ, 2));
+    let mut raw = DMatrix::zeros(SZ, SZ);
 
     rayon::ThreadPoolBuilder::new()
-        .num_threads(8)
+        .num_threads(12)
         .build_global()
         .unwrap();
 
     c.bench_function("random mismatched", |b| {
-        b.iter(|| kern.call(&x, &x).unwrap())
+        b.iter(|| kern.call(black_box(&x), black_box(&x)).unwrap())
     });
 
     c.bench_function("random symmetric", |b| {
-        b.iter(|| kern.call_symmetric_into(&x, &mut raw).unwrap())
+        b.iter(|| {
+            kern.call_symmetric_into(black_box(&x), black_box(&mut raw))
+                .unwrap()
+        })
     });
 }
 
