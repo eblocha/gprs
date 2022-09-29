d04234d67e8ce1f804794607c078f15d05b2db4a
eblocha <eblocha@protonmail.com>
2022-09-28 22:35:06 -0400

    parameterize benchmarks

--- benches/rbf_kernel.rs
+++ benches/rbf_kernel.rs
@@ -5,25 +5,44 @@ use nalgebra::DMatrix;
 fn create_random(shape: (usize, usize)) -> DMatrix<f64> {
     DMatrix::<f64>::new_random(shape.0, shape.1)
 }
-const SZ: usize = 2000;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let kern = RBF::new(vec![1.0, 2.0].iter());
-
-    let x = create_random((2, SZ));
-    let mut raw = DMatrix::zeros(SZ, SZ);
 
     rayon::ThreadPoolBuilder::new()
         .num_threads(12)
         .build_global()
         .unwrap();
 
-    c.bench_function("random mismatched", |b| {
-        b.iter(|| {
-            kern.call_into(black_box(&x), black_box(&x), black_box(&mut raw))
-                .unwrap()
-        })
-    });
+    // This does allocations
+    let mut group_call = c.benchmark_group("rbf-call");
+
+    for i in [1000 as usize, 1500, 2000, 2500, 3000].iter() {
+        group_call.throughput(criterion::Throughput::Elements(*i as u64));
+        group_call.bench_with_input(format!("{}x{}", i, i), i, |b, n| {
+            let x = create_random((2, *n));
+
+            b.iter(|| kern.call(black_box(&x), black_box(&x)).unwrap())
+        });
+    }
+    group_call.finish();
+
+    // This does not allocate
+    let mut group_call_into = c.benchmark_group("rbf-call-into");
+
+    for i in [1000 as usize, 1500, 2000, 2500, 3000].iter() {
+        group_call_into.throughput(criterion::Throughput::Elements(*i as u64));
+        group_call_into.bench_with_input(format!("{}x{}", i, i), i, |b, n| {
+            let mut raw = DMatrix::zeros(*n, *n);
+            let x = create_random((2, *n));
+
+            b.iter(|| {
+                kern.call_into(black_box(&x), black_box(&x), black_box(&mut raw))
+                    .unwrap()
+            })
+        });
+    }
+    group_call_into.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
