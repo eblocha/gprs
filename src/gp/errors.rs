e16018316afcbd4e87a2eb80bea623e3696c8e30
eblocha <eblocha@protonmail.com>
2022-09-30 19:38:40 -0400

    add extra explanation for GPCompilationError

--- src/gp/errors.rs
+++ src/gp/errors.rs
@@ -2,8 +2,10 @@ use crate::kernels::errors::IncompatibleShapeError;
 
 #[derive(Debug)]
 pub enum GPCompilationError {
-    /// The kernel returned a non-positive-definite covariance matrix for the input data
+    /// The kernel returned a non-positive-definite covariance matrix for the input data.
+    ///
+    /// This can either be caused by an invalid kernel formula, or duplicate points in the x data.
     NonPositiveDefiniteError,
-    /// The input data shape is incompatible with itself or the kernel
+    /// The input data shape is incompatible with itself or the kernel.
     IncompatibleShapeError(IncompatibleShapeError),
 }
