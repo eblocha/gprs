63ff954fa273f0b6789e66fa33bc187509157e4f
eblocha <eblocha@protonmail.com>
2022-10-08 12:37:28 -0400

    add jacobian and hessian traits

--- src/parameterized.rs
+++ src/parameterized.rs
@@ -1,3 +1,5 @@
+use nalgebra::DMatrix;
+
 pub trait Parameterized<'a> {
     /// Get the model parameters
     fn get_params(&'a self) -> Vec<f64>;
@@ -5,4 +7,14 @@ pub trait Parameterized<'a> {
     fn set_params(&'a mut self, params: &[f64]);
     /// Create a new model from parameters
     fn from_params(params: &[f64]) -> Self;
+}
+
+pub trait Jacobian<'a> {
+    /// Get the model derivative
+    fn jacobian(&'a self, x: &DMatrix<f64>) -> DMatrix<f64>;
+}
+
+pub trait Hessian<'a> {
+    /// Get the second derivative
+    fn hessian(&'a self, x: &DMatrix<f64>) -> DMatrix<f64>;
 }
