b10c440dd503a2945e8a453843bd114921b12560
eblocha <eblocha@protonmail.com>
2023-03-11 13:20:30 -0500

    Add tests for par_add_diagonal_mut_unchecked

--- src/linalg/util.rs
+++ src/linalg/util.rs
@@ -18,3 +18,32 @@ pub unsafe fn par_add_diagonal_mut_unchecked(mat: &mut DMatrix<f64>, f: &f64) {
             *(slice.get_unchecked_mut(i)) += f;
         });
 }
+
+#[cfg(test)]
+mod tests {
+    use nalgebra::DMatrix;
+
+    use super::par_add_diagonal_mut_unchecked;
+
+    #[test]
+    fn test_empty() {
+        let mut mat: DMatrix<f64> = DMatrix::from_vec(0, 0, vec![]);
+        unsafe { par_add_diagonal_mut_unchecked(&mut mat, &10.0_f64) }
+        assert_eq!(mat.as_slice(), vec![].as_slice());
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    fn test_square() {
+        let mut mat = DMatrix::from_vec(3, 3, vec![
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0]);
+        let expected = vec![
+            10.0,  0.0,  0.0,
+             0.0, 10.0,  0.0,
+             0.0,  0.0, 10.0];
+        unsafe { par_add_diagonal_mut_unchecked(&mut mat, &10.0_f64) }
+        assert_eq!(mat.as_slice(), expected.as_slice());
+    }
+}
